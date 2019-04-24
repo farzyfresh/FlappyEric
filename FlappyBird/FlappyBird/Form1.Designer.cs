6b29cf0c97679f30139a9eb6899884e3f430d02c
Shishir Salam <ssalam16@users.noreply.github.com>
2019-04-23 22:53:40 -0400

    -

--- FlappyBird/FlappyBird/Form1.Designer.cs
+++ FlappyBird/FlappyBird/Form1.Designer.cs
@@ -48,9 +48,10 @@
             // 
             this.flappyBird.BackColor = System.Drawing.Color.Transparent;
             this.flappyBird.Image = global::FlappyBird.Properties.Resources.ericgif;
-            this.flappyBird.Location = new System.Drawing.Point(48, 205);
+            this.flappyBird.Location = new System.Drawing.Point(22, 154);
+            this.flappyBird.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.flappyBird.Name = "flappyBird";
-            this.flappyBird.Size = new System.Drawing.Size(140, 83);
+            this.flappyBird.Size = new System.Drawing.Size(66, 56);
             this.flappyBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.flappyBird.TabIndex = 0;
             this.flappyBird.TabStop = false;
@@ -59,9 +60,10 @@
             // 
             this.pipeTop.BackColor = System.Drawing.Color.Transparent;
             this.pipeTop.Image = global::FlappyBird.Properties.Resources.pipedown;
-            this.pipeTop.Location = new System.Drawing.Point(308, -358);
+            this.pipeTop.Location = new System.Drawing.Point(205, -233);
+            this.pipeTop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pipeTop.Name = "pipeTop";
-            this.pipeTop.Size = new System.Drawing.Size(98, 548);
+            this.pipeTop.Size = new System.Drawing.Size(65, 356);
             this.pipeTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pipeTop.TabIndex = 1;
             this.pipeTop.TabStop = false;
@@ -70,9 +72,10 @@
             // 
             this.pipeBottom.BackColor = System.Drawing.Color.Transparent;
             this.pipeBottom.Image = global::FlappyBird.Properties.Resources.pipe;
-            this.pipeBottom.Location = new System.Drawing.Point(641, 188);
+            this.pipeBottom.Location = new System.Drawing.Point(427, 122);
+            this.pipeBottom.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pipeBottom.Name = "pipeBottom";
-            this.pipeBottom.Size = new System.Drawing.Size(98, 678);
+            this.pipeBottom.Size = new System.Drawing.Size(65, 441);
             this.pipeBottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pipeBottom.TabIndex = 2;
             this.pipeBottom.TabStop = false;
@@ -81,9 +84,10 @@
             // 
             this.ground.BackColor = System.Drawing.Color.Transparent;
             this.ground.Image = global::FlappyBird.Properties.Resources.ground;
-            this.ground.Location = new System.Drawing.Point(-2, 493);
+            this.ground.Location = new System.Drawing.Point(-1, 320);
+            this.ground.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ground.Name = "ground";
-            this.ground.Size = new System.Drawing.Size(805, 112);
+            this.ground.Size = new System.Drawing.Size(537, 73);
             this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.ground.TabIndex = 3;
             this.ground.TabStop = false;
@@ -101,9 +105,10 @@
             this.scoreText.BackColor = System.Drawing.Color.Transparent;
             this.scoreText.Font = new System.Drawing.Font("Ravie", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.scoreText.ForeColor = System.Drawing.Color.Black;
-            this.scoreText.Location = new System.Drawing.Point(25, 33);
+            this.scoreText.Location = new System.Drawing.Point(17, 21);
+            this.scoreText.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.scoreText.Name = "scoreText";
-            this.scoreText.Size = new System.Drawing.Size(0, 38);
+            this.scoreText.Size = new System.Drawing.Size(0, 26);
             this.scoreText.TabIndex = 4;
             // 
             // endText1
@@ -111,9 +116,10 @@
             this.endText1.BackColor = System.Drawing.Color.DarkKhaki;
             this.endText1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.endText1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.endText1.Location = new System.Drawing.Point(218, 294);
+            this.endText1.Location = new System.Drawing.Point(145, 191);
+            this.endText1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.endText1.Name = "endText1";
-            this.endText1.Size = new System.Drawing.Size(200, 30);
+            this.endText1.Size = new System.Drawing.Size(133, 19);
             this.endText1.TabIndex = 5;
             this.endText1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -121,9 +127,10 @@
             // 
             this.endText2.BackColor = System.Drawing.Color.DarkKhaki;
             this.endText2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.endText2.Location = new System.Drawing.Point(218, 339);
+            this.endText2.Location = new System.Drawing.Point(145, 220);
+            this.endText2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.endText2.Name = "endText2";
-            this.endText2.Size = new System.Drawing.Size(200, 30);
+            this.endText2.Size = new System.Drawing.Size(133, 19);
             this.endText2.TabIndex = 6;
             this.endText2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -131,18 +138,19 @@
             // 
             this.gameDesigners.BackColor = System.Drawing.Color.DarkKhaki;
             this.gameDesigners.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gameDesigners.Location = new System.Drawing.Point(168, 388);
+            this.gameDesigners.Location = new System.Drawing.Point(112, 252);
+            this.gameDesigners.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.gameDesigners.Name = "gameDesigners";
-            this.gameDesigners.Size = new System.Drawing.Size(300, 30);
+            this.gameDesigners.Size = new System.Drawing.Size(200, 19);
             this.gameDesigners.TabIndex = 7;
             this.gameDesigners.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // flappyGame
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::FlappyBird.Properties.Resources.background;
-            this.ClientSize = new System.Drawing.Size(805, 603);
+            this.ClientSize = new System.Drawing.Size(537, 392);
             this.Controls.Add(this.gameDesigners);
             this.Controls.Add(this.endText2);
             this.Controls.Add(this.endText1);
@@ -151,6 +159,7 @@
             this.Controls.Add(this.pipeBottom);
             this.Controls.Add(this.pipeTop);
             this.Controls.Add(this.flappyBird);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "flappyGame";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.inGameKeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.inGameKeyUP);
